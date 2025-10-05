438a9e49f4063579dbf4ed003420774011ce0633
mitch-scott372 <mitch@mybigsky.org>
2025-10-05 12:50:18 +1100

    function_ms added

---
+++ scripts/functions_MS.py
@@ -0,0 +1,20 @@
+from uncertainties import ufloat, nominal_value, std_dev
+import numpy as np
+
+def get_series(df, region):
+    """Return time (weeks), nominal values, and uncertainties for a single country."""
+    if region not in df.index:
+        raise ValueError(f"{region} not found in DataFrame index")
+
+    row = df.loc[region]  # Series of ufloats
+    row = row.iloc[2:]
+    time = np.arange(len(row))
+    values = np.array([nominal_value(v) for v in row])
+    errors = np.array([std_dev(v) for v in row])
+    return time, values, errors
+
+def get_ECW(df, region):
+    row = df.loc[region]
+    ECW_Upper = row[1]
+    ECW_Lower = row[0]
+    return ECW_Lower, ECW_Upper
